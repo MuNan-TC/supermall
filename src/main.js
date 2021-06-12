8bcbafa210c9d61af7b9f0f1208d3a0d37a7ccf8
MuNan-TC <2683098165@qq.com>
2021-06-12 15:34:48 +0800

    vueLazyLoad

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 import toast from './components/common/toast'
+import VueLazyLoad from 'vue-lazyload'
 
 Vue.config.productionTip = false
 
@@ -10,6 +12,11 @@ Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
 //注册toast插件
 Vue.use(toast)
+
+//使用图片懒加载插件
+Vue.use(VueLazyLoad, {
+  loading: require('./assets/img/common/placeholder.png')
+})
 
 new Vue({
   render: h => h(App),
